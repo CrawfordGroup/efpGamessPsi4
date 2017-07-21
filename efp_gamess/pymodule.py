16c795b2b49956e7fd8b96d5322a4f005d99e4a6
jH0ward <jchoward88@gmail.com>
2017-07-21 11:15:34 -0400

    Getting transformation matrices from C++. Ready for manipulation in python

--- efp_gamess/pymodule.py
+++ efp_gamess/pymodule.py
@@ -1,7 +1,7 @@
 #
 # @BEGIN LICENSE
 #
-# efp_gamess by Psi4 Developer, a plugin to:
+# gamess_h5 by Psi4 Developer, a plugin to:
 #
 # Psi4: an open-source quantum chemistry software package
 #
@@ -31,6 +31,8 @@
 import psi4
 import psi4.driver.p4util as p4util
 from psi4.driver.procrouting import proc_util
+import h5py
+import numpy as np
 
 def run_efp_gamess(name, **kwargs):
     r"""Function encoding sequence of PSI module and plugin calls so that
@@ -57,9 +59,72 @@ def run_efp_gamess(name, **kwargs):
 
     # Call the Psi4 plugin
     # Please note that setting the reference wavefunction in this way is ONLY for plugins
-    efp_gamess_wfn = psi4.core.plugin('efp_gamess.so', ref_wfn)
+    # This prints a bound python method
+    print("\nPrinting ref_wfn.Fa")
+    print(ref_wfn.Fa)
+    # This defines a psi4.core.Matrix object and prints it
+    print(np.asarray(ref_wfn.Fa()))
+    #Farray=np.asarray(ref_wfn.Fa())
+    Farray=ref_wfn.Fa().to_array()
+    print("\nPrinting Farray, which is np.asarray(ref_wfn.Fa())")
+    print(Farray)
 
-    return efp_gamess_wfn
+    print("\nPrinting Farray asarray")
+    print(np.asarray(Farray))
+    # I think this does nothing different from the above
+    Fa=ref_wfn.Fa()
+    print("\nPrinting Fa=ref_wfn.Fa()")
+    print(Fa)
+    print("\nPrinting np.asarray(Fa)")
+    print(np.asarray(Fa))
+    # This prints to the output the matrix values
+    Fa.print_out()
+    # This actually changes Fa and the ref_wfn Fa value
+    Fa.set(0,0,0.0)
+    Fa.print_out()
+    #ref_wfn.Fa=Fa
+    # Tests my previous assertion about ref_wfn changes
+    Fa_zeroed=ref_wfn.Fa()
+    Fa_zeroed.print_out()
+
+    # Change return name to the trans matrix I'm trying to send back
+    #efp_gamess_wfn = psi4.core.plugin('efp_gamess.so', ref_wfn)
+
+    
+    psi4.core.set_local_option('efp_gamess', 'coleman', 0)
+
+    trans_mat_c=psi4.core.plugin('efp_gamess.so',ref_wfn)
+    print("\nPrinting C transformation matrix")
+    print(trans_mat_c)
+    print("\nPrinting C transformation matrix (as array)")
+    print(np.asarray(trans_mat_c))
+    trans_mat_c.print_out()
+
+    psi4.core.print_out("\nalright coleman H coming now")
+    psi4.core.set_local_option('efp_gamess', 'coleman', 1)
+
+    trans_mat_h=psi4.core.plugin('efp_gamess.so',ref_wfn)
+    print("\nPrinting H transformation matrix")
+    print(trans_mat_h)
+    print("\nPrinting H transformation matrix (as array)")
+    print(np.asarray(trans_mat_h))
+    trans_mat_h.print_out()
+
+    # Test adding these together
+    print("Adding np.asarray(trans_mat_h)+np.asarray(trans_mat_c)\n")
+    test_sum=np.asarray(trans_mat_h)+np.asarray(trans_mat_c)
+    print(test_sum)
+    #test_sum2=np.asarray(Fa)+np.asarray(trans_mat_c)
+    #print(test_sum2)
+    #test_sum.print_out()
+    # Find out what trans_mat is (it's like the others)
+    #print(trans_mat)
+    # print actual values
+    #trans_mat.print_out()
+
+    # Change return to original ref_wfn, hopefully changed
+    #return efp_gamess_wfn
+    return ref_wfn
 
 
 # Integration with driver routines

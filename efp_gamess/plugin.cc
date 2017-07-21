3da116dcc534c83fb6cee3bc4e0cb5df7ce3cbe8
jH0ward <jchoward88@gmail.com>
2017-07-21 10:34:06 -0400

    Initial commit of unedited new psi4 plugin

---
+++ efp_gamess/plugin.cc
@@ -0,0 +1,61 @@
+/*
+ * @BEGIN LICENSE
+ *
+ * efp_gamess by Psi4 Developer, a plugin to:
+ *
+ * Psi4: an open-source quantum chemistry software package
+ *
+ * Copyright (c) 2007-2017 The Psi4 Developers.
+ *
+ * The copyrights for code used from other parties are included in
+ * the corresponding files.
+ *
+ * This file is part of Psi4.
+ *
+ * Psi4 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Psi4 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with Psi4; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * @END LICENSE
+ */
+
+#include "psi4/psi4-dec.h"
+#include "psi4/libparallel/parallel.h"
+#include "psi4/liboptions/liboptions.h"
+#include "psi4/libpsio/psio.hpp"
+
+namespace psi{ namespace efp_gamess {
+
+extern "C"
+int read_options(std::string name, Options& options)
+{
+    if (name == "EFP_GAMESS"|| options.read_globals()) {
+        /*- The amount of information printed to the output file -*/
+        options.add_int("PRINT", 1);
+    }
+
+    return true;
+}
+
+extern "C"
+SharedWavefunction efp_gamess(SharedWavefunction ref_wfn, Options& options)
+{
+    int print = options.get_int("PRINT");
+
+    /* Your code goes here */
+
+    // Typically you would build a new wavefunction and populate it with data
+    return ref_wfn;
+}
+
+}} // End namespaces
+
